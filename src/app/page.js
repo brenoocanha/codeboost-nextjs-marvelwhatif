5587b37cb8755568f3c487fabe819ab908d9f6f3
Breno Ocanha <breno@dmxdesign.com.br>
2023-09-26 16:08:49 -0300

    feat: tailwind color variables

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/button"
 export default function Home() {
   return (
     <>
-      <h1 className="text-5xl">What If Codeboost</h1>
+      <h1 className="text-5xl text-red9">What If Codeboost</h1>
       <Button />
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,42 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      mobile: '0px',
+      // => @media (min-width: 768px) { ... }
+
+      tablet: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      full: '1920',
+      // => @media (min-width: 1920px) { ... }
+
+      ultrawide: '2560',
+      // => @media (min-width: 2560px) { ... }
+    },
+    extend: {
+      colors: {
+        "red9": "var(--red-9)",
+        "red8": "var(--red-8)",
+        "gray9": "var(--gray-9)",
+        "gray8": "var(--gray-8)",
+        "gray7": "var(--gray-7)",
+        "gray6": "var(--gray-6)",
+        "gray5": "var(--gray-5)",
+        "gray4": "var(--gray-4)",
+        "gray2": "var(--gray-2)",
+        "gray1": "var(--gray-1)",
+      },
+      backgroundImage: {
+
+      },
+    },
   },
   plugins: [],
 }
